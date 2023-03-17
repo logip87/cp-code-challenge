cb6f70b7c56506c1a60859987af4b8a1b3be83e6
pawel.logasz <p.logasz@gmail.com>
2023-03-17 12:51:25 +0100

    Add login assertions

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,5 +1,6 @@
 import { USERS } from "./constants";
 import { loginPageSelectors } from "./selectors/loginPage";
+import { mainPageSelectors } from "./selectors/mainPage";
 
 Cypress.Commands.add("login", (role) => {
   const username = USERS[role].username;
@@ -11,6 +12,8 @@ Cypress.Commands.add("login", (role) => {
     .should("be.visible")
     .type(password);
   cy.get(loginPageSelectors.buttons.submit).should("be.visible").click();
+  cy.contains("Products").should("be.visible");
+  cy.get(mainPageSelectors.inventory.item).should("have.length.above", 2);
 });
 
 Cypress.Commands.add("inputHasError", (selector, value = true) => {
