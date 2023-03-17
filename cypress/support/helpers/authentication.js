158b5827861b2185a63d756871c590fc79b6141c
pawel.logasz <p.logasz@gmail.com>
2023-03-17 12:52:00 +0100

    Refractor enter login credentials function

--- cypress/support/helpers/authentication.js
+++ cypress/support/helpers/authentication.js
@@ -1,20 +1,13 @@
 import { loginPageSelectors } from "../selectors/loginPage";
 
 export function enterLoginCredentials(username, password) {
-  if (password == "") {
+  if (username != "") {
     cy.get(loginPageSelectors.inputs.username)
       .should("be.visible")
       .type(username);
     cy.get(loginPageSelectors.buttons.submit).should("be.visible").click();
-  } else if (username == "") {
-    cy.get(loginPageSelectors.inputs.password)
-      .should("be.visible")
-      .type(password);
-    cy.get(loginPageSelectors.buttons.submit).should("be.visible").click();
-  } else {
-    cy.get(loginPageSelectors.inputs.username)
-      .should("be.visible")
-      .type(username);
+  }
+  if (password != "") {
     cy.get(loginPageSelectors.inputs.password)
       .should("be.visible")
       .type(password);
